31132d04eb7b57410c621488a73d2898f6c013bb
Aditya Soni <adityas@geekyants.com>
2020-06-04 14:52:11 +0530

    added server client and send/recieve thread classes

--- android/src/main/java/com/aditya/flutterbluetoothadapter/FlutterbluetoothadapterPlugin.java
+++ android/src/main/java/com/aditya/flutterbluetoothadapter/FlutterbluetoothadapterPlugin.java
@@ -3,11 +3,19 @@ package com.aditya.flutterbluetoothadapter;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Message;
 
 import androidx.annotation.NonNull;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -26,12 +34,44 @@ public class FlutterbluetoothadapterPlugin implements FlutterPlugin, MethodCallH
     BluetoothDevice[] btDevices;
     Registrar activityRegistrar;
     Activity activity;
+    SendRecieve sendRecieve;
     static final int STATE_LISTENING = 1;
     static final int STATE_CONNECTING = 2;
     static final int STATE_CONNECTED = 3;
     static final int STATE_CONNECTION_FAILED = 4;
     static final int STATE_MESSAGE_RECEIVED = 5;
+    private static final String APP_NAME = "BtChat";
+    private static final UUID MY_UUID = UUID.fromString("20585adb-d260-445e-934b-032a2c8b2e14");
 
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case STATE_LISTENING:
+//                    status.setText("Listening");
+                    break;
+
+                case STATE_CONNECTING:
+//                    status.setText("Connecting");
+                    break;
+
+                case STATE_CONNECTED:
+//                    status.setText("Connected");
+                    break;
+
+                case STATE_CONNECTION_FAILED:
+//                    status.setText("Connection Failed");
+                    break;
+
+                case STATE_MESSAGE_RECEIVED:
+//                    byte[] readBuffer = (byte[]) msg.obj;
+//                    String tempMsg = new String(readBuffer, 0, msg.arg1);
+//                    message.setText(tempMsg);
+                    break;
+            }
+            return true;
+        }
+    });
 
     public FlutterbluetoothadapterPlugin() {
         this.activityRegistrar = null;
@@ -108,5 +148,116 @@ public class FlutterbluetoothadapterPlugin implements FlutterPlugin, MethodCallH
             }
         }
         return "Unknown status";
+    }
+
+    private class ServerClass extends Thread {
+        private BluetoothServerSocket serverSocket;
+
+        public ServerClass() {
+            try {
+                serverSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(APP_NAME, MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            while (socket == null) {
+                try {
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTING;
+                    handler.sendMessage(message);
+                    socket = serverSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTION_FAILED;
+                    handler.sendMessage(message);
+                }
+                if (socket != null) {
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTED;
+                    handler.sendMessage(message);
+                    // write some code for send recieve message here
+                    sendRecieve = new SendRecieve(socket);
+                    sendRecieve.start();
+                    break;
+                }
+            }
+        }
+    }
+
+    private class ClientClass extends Thread {
+        private BluetoothDevice device;
+        private BluetoothSocket socket;
+
+        public ClientClass(BluetoothDevice device1) {
+            device = device1;
+            try {
+                socket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            try {
+                socket.connect();
+                Message message = Message.obtain();
+                message.what = STATE_CONNECTED;
+                handler.sendMessage(message);
+                sendRecieve = new SendRecieve(socket);
+                sendRecieve.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Message message = Message.obtain();
+                message.what = STATE_CONNECTION_FAILED;
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    private class SendRecieve extends Thread {
+        private final BluetoothSocket bluetoothSocket;
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+
+        public SendRecieve(BluetoothSocket socket) {
+            bluetoothSocket = socket;
+            InputStream tempInputStr = null;
+            OutputStream tempOutputStr = null;
+
+            try {
+                tempInputStr = bluetoothSocket.getInputStream();
+                tempOutputStr = bluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            inputStream = tempInputStr;
+            outputStream = tempOutputStr;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = inputStream.read(buffer);
+                    handler.obtainMessage(STATE_MESSAGE_RECEIVED, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void write(byte[] bytes) {
+            try {
+                outputStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
