2b9fa596cd6237379b598fc8c4c7233ad411eb83
Aditya Soni <adityasoni5859@gmail.com>
2020-06-16 14:52:37 +0530

    code refactor

--- test/flutterbluetoothadapter_test.dart
+++ test/flutterbluetoothadapter_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutterbluetoothadapter/flutterbluetoothadapter.dart';
+import 'package:flutter_bluetooth_adapter/flutterbluetoothadapter.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutterbluetoothadapter');
